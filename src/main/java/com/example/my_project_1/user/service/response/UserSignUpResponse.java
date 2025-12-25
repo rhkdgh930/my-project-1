9879016226757aea57ef69f4257642283489094c
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2025-12-26 04:00:46 +0900

    feat: [service] 유저 서비스 코드 작성

---
+++ src/main/java/com/example/my_project_1/user/service/response/UserSignUpResponse.java
@@ -0,0 +1,24 @@
+package com.example.my_project_1.user.service.response;
+
+import com.example.my_project_1.user.domain.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class UserSignUpResponse {
+    private Long id;
+    private String email;
+    private String nickname;
+
+    public static UserSignUpResponse from(User user) {
+        UserSignUpResponse response = new UserSignUpResponse();
+        response.id = user.getId();
+        response.email = user.getEmail();
+        response.nickname = user.getNickname();
+        return response;
+    }
+}
+
