1edb5efe5c1889216e257ad3293ddcc645a2965b
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2025-12-26 05:52:47 +0900

    refactor: 정책 변경으로 인한 PasswordEncoder.java 삭제

--- src/main/java/com/example/my_project_1/user/domain/PasswordEncoder.java
+++
@@ -1,7 +0,0 @@
-package com.example.my_project_1.user.domain;
-
-public interface PasswordEncoder {
-    String encode(String password);
-
-    boolean matches(String password, String passwordHash);
-}
--- src/main/java/com/example/my_project_1/user/service/UserServiceImpl.java
+++ src/main/java/com/example/my_project_1/user/service/UserServiceImpl.java
@@ -2,12 +2,12 @@ package com.example.my_project_1.user.service;
 
 import com.example.my_project_1.common.exception.CustomException;
 import com.example.my_project_1.common.exception.ErrorCode;
-import com.example.my_project_1.user.domain.PasswordEncoder;
 import com.example.my_project_1.user.domain.User;
 import com.example.my_project_1.user.service.request.UserSignUpRequest;
 import com.example.my_project_1.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
