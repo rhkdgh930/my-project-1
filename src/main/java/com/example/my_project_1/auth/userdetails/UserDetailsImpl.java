90256b0454ca0abd3d5eee4c3b937465bdcb6182
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2025-12-30 01:28:54 +0900

    feat: UserDetailImpl 구현

---
+++ src/main/java/com/example/my_project_1/auth/userdetails/UserDetailsImpl.java
@@ -0,0 +1,63 @@
+package com.example.my_project_1.auth.userdetails;
+
+
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private Long userId;
+    private String email;
+    private String password;
+    private String role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.role));
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    //    @Override
+//    public boolean isAccountNonExpired() {
+//        return UserDetails.super.isAccountNonExpired();
+//    }
+//
+//    @Override
+//    public boolean isAccountNonLocked() {
+//        return UserDetails.super.isAccountNonLocked();
+//    }
+//
+//    @Override
+//    public boolean isCredentialsNonExpired() {
+//        return UserDetails.super.isCredentialsNonExpired();
+//    }
+//
+//    @Override
+//    public boolean isEnabled() {
+//        return UserDetails.super.isEnabled();
+//    }
+}
---
+++ src/main/java/com/example/my_project_1/auth/userdetails/UserDetailsServiceImpl.java
@@ -0,0 +1,24 @@
+package com.example.my_project_1.auth.userdetails;
+
+import com.example.my_project_1.user.domain.Role;
+import com.example.my_project_1.user.domain.User;
+import com.example.my_project_1.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepository.findByEmail(email).orElseThrow(
+                () -> new UsernameNotFoundException("존재하지 않는 사용자입니다.")
+        );
+        return new UserDetailsImpl(user.getId(), user.getEmail(), user.getPassword(), user.getRole().name());
+    }
+}
