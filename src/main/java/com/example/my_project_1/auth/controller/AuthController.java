19e2ccf80facdfb8ba79038eebd8a9afa8a9643b
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2025-12-30 01:32:47 +0900

    feat: Auth 관련 컨트롤러 정의

---
+++ src/main/java/com/example/my_project_1/auth/controller/AuthController.java
@@ -0,0 +1,29 @@
+package com.example.my_project_1.auth.controller;
+
+import com.example.my_project_1.auth.service.response.TokenResponse;
+import com.example.my_project_1.auth.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping("/reissue")
+    public ResponseEntity<TokenResponse> reissue(@RequestHeader("Refresh-Token") String refreshToken) {
+        TokenResponse tokenResponse = authService.reissue(refreshToken);
+        return ResponseEntity.ok(tokenResponse);
+    }
+
+    @PostMapping("/logout")
+    public ResponseEntity<Void> logout(@RequestHeader("Authorization") String accessToken) {
+        authService.logout(accessToken.replace("Bearer ", ""));
+        return ResponseEntity.noContent().build();
+    }
+}
