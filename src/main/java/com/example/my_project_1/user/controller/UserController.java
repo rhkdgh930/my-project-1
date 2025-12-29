dcf34658af18cdf0c20e542d269ae36a7f36b76c
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2025-12-30 01:33:31 +0900

    feat: 유저 컨트롤러 추가

--- src/main/java/com/example/my_project_1/user/controller/UserController.java
+++ src/main/java/com/example/my_project_1/user/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.example.my_project_1.user.controller;
 
+import com.example.my_project_1.auth.userdetails.UserDetailsImpl;
+import com.example.my_project_1.common.utils.DataSerializer;
 import com.example.my_project_1.user.domain.User;
 import com.example.my_project_1.user.service.request.UserSignUpRequest;
 import com.example.my_project_1.user.service.response.UserSignUpResponse;
@@ -8,10 +10,10 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,5 +25,17 @@ public class UserController {
     public ResponseEntity<UserSignUpResponse> signUp(@Valid @RequestBody UserSignUpRequest request) {
         User user = userService.signUp(request);
         return ResponseEntity.status(HttpStatus.CREATED).body(UserSignUpResponse.from(user));
+    }
+
+    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/test/user")
+    public ResponseEntity<UserDetails> getMyInfo(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(userDetails);
+    }
+
+    @PreAuthorize("hasRole('ADMIN')")
+    @GetMapping("/test/admin")
+    public ResponseEntity<UserDetails> getMyInfo2(@AuthenticationPrincipal UserDetails userDetails) {
+        return ResponseEntity.ok(userDetails);
     }
 }
