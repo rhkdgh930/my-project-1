6297af14634f2698ace72284594beb357872f18e
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2025-12-30 01:30:42 +0900

    feat: SecurityConfig 구현

--- src/main/java/com/example/my_project_1/auth/config/SecurityConfig.java
+++ src/main/java/com/example/my_project_1/auth/config/SecurityConfig.java
@@ -1,36 +1,73 @@
 package com.example.my_project_1.auth.config;
 
+import com.example.my_project_1.auth.filter.*;
+import com.example.my_project_1.auth.service.RedisTokenService;
+import com.example.my_project_1.auth.utils.JwtProvider;
+import com.example.my_project_1.auth.utils.UrlUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JwtProvider jwtProvider;
+    private final RedisTokenService redisTokenService;
+    private final UserDetailsService userDetailsService;
+    private final AuthenticationConfiguration authenticationConfiguration;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        AuthenticationManager authenticationManager = authenticationConfiguration.getAuthenticationManager();
+
+        JwtLoginSuccessHandler successHandler = new JwtLoginSuccessHandler(jwtProvider, redisTokenService);
+
+        JwtLoginFailureHandler failureHandler = new JwtLoginFailureHandler();
+
+        JwtLoginFilter loginFilter = new JwtLoginFilter(authenticationManager, successHandler, failureHandler);
+
+        JwtAuthenticationFilter authenticationFilter = new JwtAuthenticationFilter(jwtProvider, redisTokenService, userDetailsService);
+
         http
                 .csrf(csrf -> csrf.disable())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/api/user/**").permitAll()
+
+                .exceptionHandling(ex -> ex
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+                )
+
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(UrlUtils.PERMITTED).permitAll()
+                        .requestMatchers("/admin/**").hasRole("ADMIN")
+                        .requestMatchers("/api/user/**").hasAnyRole("USER", "ADMIN")
                         .anyRequest().authenticated()
-                );
+                )
+
+                .addFilterAt(loginFilter, UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
+
         return http.build();
     }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
---
+++ src/main/java/com/example/my_project_1/auth/utils/UrlUtils.java
@@ -0,0 +1,12 @@
+package com.example.my_project_1.auth.utils;
+
+public class UrlUtils {
+    public static final String[] PERMITTED = {
+            //auth
+            "/api/auth/login",
+            "/api/auth/reissue",
+
+            //user
+            "/api/user/signup"
+    };
+}
