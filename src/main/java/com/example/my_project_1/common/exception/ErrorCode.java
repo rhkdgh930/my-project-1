1e827fff751431daf09446df5bf409af41fd5388
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2025-12-30 01:28:06 +0900

    feat: ErrorCode 추가

--- src/main/java/com/example/my_project_1/common/exception/ErrorCode.java
+++ src/main/java/com/example/my_project_1/common/exception/ErrorCode.java
@@ -19,13 +19,19 @@ public enum ErrorCode {
     INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "입력 값이 유효하지 않습니다. 필수값을 확인해주세요."),
     DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
 
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
+    INVALID_TOKEN_TYPE(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰 타입입니다."),
+    EXPIRED_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 토큰입니다."),
+    REVOKED_TOKEN(HttpStatus.UNAUTHORIZED, "로그아웃된 토큰입니다."),
+
+    INVALID_CREDENTIALS(HttpStatus.BAD_REQUEST, "이메일 또는 비밀번호가 올바르지 않습니다."),
+
     /**
      * 500:
      */
-    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 알 수 없는 오류가 발생했습니다."),
+    INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 알 수 없는 오류가 발생했습니다.");
 
 
-    ;
     private final HttpStatus httpStatus;
     private final String message;
 }
