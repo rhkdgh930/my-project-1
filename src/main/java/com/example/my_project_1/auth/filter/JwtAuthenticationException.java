b30b78c08adf015f87a0ef22924f4b248d03699a
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2025-12-30 01:31:54 +0900

    feat: jwt관련 에러 정의

---
+++ src/main/java/com/example/my_project_1/auth/filter/JwtAuthenticationException.java
@@ -0,0 +1,17 @@
+package com.example.my_project_1.auth.filter;
+
+import com.example.my_project_1.common.exception.ErrorCode;
+import org.springframework.security.core.AuthenticationException;
+
+public class JwtAuthenticationException extends AuthenticationException {
+    private final ErrorCode errorCode;
+
+    public JwtAuthenticationException(ErrorCode errorCode) {
+        super(errorCode.getMessage());
+        this.errorCode = errorCode;
+    }
+
+    public ErrorCode getErrorCode() {
+        return errorCode;
+    }
+}
