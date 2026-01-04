a3d7150473bce1a6f0715f81f8ae93f4b9c77a87
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2026-01-05 03:10:27 +0900

    refactor: 에러코드 수정

--- src/main/java/com/example/my_project_1/common/exception/ErrorCode.java
+++ src/main/java/com/example/my_project_1/common/exception/ErrorCode.java
@@ -22,10 +22,9 @@ public enum ErrorCode {
 
     // 추가: 계정 상태 관련
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
-    USER_SUSPENDED(HttpStatus.UNAUTHORIZED, "정지된 계정입니다."),
 
     /* 401 UNAUTHORIZED: 인증 실패 */
-    LOGOUT_USER(HttpStatus.UNAUTHORIZED, "로그아웃된 계정입니다. 다시 로그인해주세요."),
+    LOGOUT_USER(HttpStatus.UNAUTHORIZED, "다시 로그인해주세요."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 올바르지 않습니다."),
 
     INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
@@ -36,6 +35,7 @@ public enum ErrorCode {
 
     /* 403 FORBIDDEN: 권한 없음 */
     ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근 권한이 없습니다."),
+    USER_SUSPENDED(HttpStatus.FORBIDDEN, "정지된 계정입니다."),
 
     /* 500 INTERNAL_SERVER_ERROR: 서버 내부 오류 */
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에서 알 수 없는 오류가 발생했습니다.");
