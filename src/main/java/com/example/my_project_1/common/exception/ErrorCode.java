04537fedb927f27b49b6d6ec4fa23a8cba036855
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2026-01-05 01:16:41 +0900

    refactor: 에러코드 수정

--- src/main/java/com/example/my_project_1/common/exception/ErrorCode.java
+++ src/main/java/com/example/my_project_1/common/exception/ErrorCode.java
@@ -18,18 +18,19 @@ public enum ErrorCode {
     DUPLICATED_EMAIL(HttpStatus.BAD_REQUEST, "이미 존재하는 이메일입니다."),
 
     // 추가: 이미 처리된 상태에 대한 요청
-    ALREADY_VERIFIED_USER(HttpStatus.BAD_REQUEST, "이미 인증이 완료된 사용자입니다."),
-    ALREADY_WITHDRAWN_USER(HttpStatus.BAD_REQUEST, "이미 탈퇴한 사용자입니다."),
+    ALREADY_VERIFIED_USER(HttpStatus.BAD_REQUEST, "이미 이메일 인증이 완료된 사용자입니다."),
 
     // 추가: 계정 상태 관련
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "존재하지 않는 사용자입니다."),
-    USER_SUSPENDED(HttpStatus.FORBIDDEN, "정지된 계정입니다. 관리자에게 문의하세요."),
+    USER_SUSPENDED(HttpStatus.UNAUTHORIZED, "정지된 계정입니다."),
 
     /* 401 UNAUTHORIZED: 인증 실패 */
     LOGOUT_USER(HttpStatus.UNAUTHORIZED, "로그아웃된 계정입니다. 다시 로그인해주세요."),
     INVALID_CREDENTIALS(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 올바르지 않습니다."),
+
+    INVALID_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 토큰입니다."),
     INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 엑세스 토큰입니다."),
-    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "재발급에 유효하지 않은 리프레시 토큰입니다."),
+    INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
     EXPIRED_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 엑세스 토큰입니다."),
     EXPIRED_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "만료된 리프레시 토큰입니다."),
 
