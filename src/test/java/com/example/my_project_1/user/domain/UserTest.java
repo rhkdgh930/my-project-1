00e2b0a8f4d244606ff09d6e69f856543f16d6b6
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2026-01-06 03:58:24 +0900

    test: 테스트코드 추가

--- src/test/java/com/example/my_project_1/user/domain/UserTest.java
+++ src/test/java/com/example/my_project_1/user/domain/UserTest.java
@@ -1,5 +1,6 @@
 package com.example.my_project_1.user.domain;
 
+import com.example.my_project_1.common.exception.CustomException;
 import com.example.my_project_1.user.service.request.UserSignUpRequest;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -28,8 +29,8 @@ class UserTest {
     }
 
     @Test
-    @DisplayName("닉네임 변경 성공 테스트")
-    void updateNicknameTest() {
+    @DisplayName("닉네임 변경을 성공한다.")
+    void updateNickname_success_test() {
         User user = getUser();
         assertThat(user.getNickname()).isEqualTo(NICKNAME);
         String changedNickname = "changedNickname";
@@ -39,8 +40,8 @@ class UserTest {
     }
 
     @Test
-    @DisplayName("비밀번호 변경 성공 테스트")
-    void updatePasswordTest() {
+    @DisplayName("비밀번호 변경을 성공한다.")
+    void updatePassword_success_test() {
         User user = getUser();
         assertThat(user.getPassword()).isEqualTo(PASSWORD);
         String changedPassword = "changedPassword";
@@ -50,8 +51,8 @@ class UserTest {
     }
 
     @Test
-    @DisplayName("유저 삭제 테스트")
-    void withdrawTest() {
+    @DisplayName("유저 삭제를 성공한다.")
+    void delete_success_test() {
         User user = getUser();
         assertThat(user.isDeleted()).isFalse();
 
@@ -60,8 +61,8 @@ class UserTest {
     }
 
     @Test
-    @DisplayName("유저 이메일 인증 테스트")
-    void verifyEmailTest() {
+    @DisplayName("유저 이메일 인증을 성공한다.")
+    void verifyEmail_success_test() {
         User user = getUser();
         assertThat(user.isEmailVerified()).isFalse();
         assertThat(user.getUserStatus()).isEqualTo(UserStatus.PENDING);
@@ -69,6 +70,55 @@ class UserTest {
         user.verifyEmail();
         assertThat(user.isEmailVerified()).isTrue();
         assertThat(user.getUserStatus()).isEqualTo(UserStatus.ACTIVE);
+    }
+
+    @DisplayName("유저 차단을 성공한다. ")
+    @Test
+    void suspend_success_test() {
+        User user = getUser();
+        assertThat(user.getAccountStatus()).isEqualTo(AccountStatus.NORMAL);
+        assertThat(user.isDeleted()).isFalse();
+
+        user.suspend();
+
+        assertThat(user.getAccountStatus()).isEqualTo(AccountStatus.SUSPENDED);
+        assertThat(user.isDeleted()).isFalse();
+    }
+
+    @DisplayName("삭제된 유저는 차단에 실패한다.")
+    @Test
+    void suspend_fail_test_already_deleted() {
+        User user = getUser();
+        user.delete();
+
+        assertThat(user.isDeleted()).isTrue();
+        assertThatThrownBy(() -> user.suspend())
+                .isInstanceOf(CustomException.class)
+                .hasMessage("존재하지 않는 사용자입니다.");
+    }
+
+    @DisplayName("이미 차단된 유저는 차단에 실패한다.")
+    @Test
+    void suspend_fail_test_already_suspended() {
+        User user = getUser();
+        user.suspend();
+
+        assertThatThrownBy(() -> user.suspend())
+                .isInstanceOf(CustomException.class)
+                .hasMessage("차단된 계정입니다.");
+    }
+
+    @DisplayName("삭제되지 않고 이메일 인증을 완료한 유저는 활성화 상태입니다.")
+    @Test
+    void isActive_success_test() {
+        User user = getUser();
+        assertThat(user.getUserStatus()).isEqualTo(UserStatus.PENDING);
+        assertThat(user.getAccountStatus()).isEqualTo(AccountStatus.NORMAL);
+
+        user.verifyEmail();
+        assertThat(user.getUserStatus()).isEqualTo(UserStatus.ACTIVE);
+
+        assertThat(user.isActive()).isTrue();
     }
 
     private static User getUser() {
