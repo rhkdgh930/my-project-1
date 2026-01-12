0af3425b0f13f48db3df2fa0363b0ba80cfee1b6
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2026-01-13 01:56:23 +0900

    feat: 추후 msa로 확장을 고려한 userClient로 나누기

---
+++ src/main/java/com/example/my_project_1/user/client/LocalUserClient.java
@@ -0,0 +1,29 @@
+package com.example.my_project_1.user.client;
+
+import com.example.my_project_1.user.domain.User;
+import com.example.my_project_1.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+@Component
+@RequiredArgsConstructor
+public class LocalUserClient implements UserClient {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public Map<Long, UserSummary> findUsersByIds(List<Long> ids) {
+        return userRepository.findAllById(ids).stream()
+                .collect(Collectors.toMap(
+                        User::getId,
+                        user -> new UserSummary(
+                                user.getId(),
+                                user.getNickname()
+                        )
+                ));
+    }
+}
---
+++ src/main/java/com/example/my_project_1/user/client/UserSummary.java
@@ -0,0 +1,7 @@
+package com.example.my_project_1.user.client;
+
+public record UserSummary(
+        Long userId,
+        String nickname
+) {
+}
