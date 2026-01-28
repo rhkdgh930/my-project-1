9b43c6bad15e6ea70c37fda166ebfba8fc0a42f7
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2026-01-28 17:12:38 +0900

    codeStyle: 코드 정리

--- src/main/java/com/example/my_project_1/auth/constant/SecurityConstants.java
+++ src/main/java/com/example/my_project_1/auth/constant/SecurityConstants.java
@@ -3,5 +3,6 @@ package com.example.my_project_1.auth.constant;
 public final class SecurityConstants {
     public static final String AUTHORIZATION = "Authorization";
     public static final String BEARER = "Bearer ";
+    public static final String REFRESH_TOKEN = "Refresh-Token";
 
 }
