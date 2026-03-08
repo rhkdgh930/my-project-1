83252950e24be2ab0cd2c2196eda7446536592e4
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2026-03-09 02:26:37 +0900

    refactor: validate 옵션 dto에 추가

--- src/main/java/com/example/my_project_1/user/service/request/UserProfileUpdateRequest.java
+++ src/main/java/com/example/my_project_1/user/service/request/UserProfileUpdateRequest.java
@@ -1,10 +1,14 @@
 package com.example.my_project_1.user.service.request;
 
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import org.hibernate.validator.constraints.URL;
 
 @Getter
 public class UserProfileUpdateRequest {
+    @Size(max = 500)
     private String introduce;
 
+    @URL
     private String profileImageUrl;
 }
