bdb5cc26fc63a5f9064895ad6a4aff31dad8024b
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2026-04-29 00:49:26 +0900

    fix: lastLoginAt 컬럼 nullable 조건 false로 수정에 따른 data.sql 수정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO users (email, password, nickname, role, user_status, account_status, is_email_verified, created_at, updated_at, introduce, profile_image_url)
-VALUES ('super@super.com', '{bcrypt}$2a$10$2Jk..KA5IiOebrrUgexVAOnY42jqryCRbMKT6FIWTXYJean7w2hUS', 'super', 'ADMIN', 'ACTIVE', 'NORMAL', true, NOW(), NOW(), '자기소개를 입력해주세요.', 'uploads/default.png');
+INSERT INTO users (email, password, nickname, role, user_status, account_status, is_email_verified, created_at, updated_at, introduce, profile_image_url, last_login_at)
+VALUES ('super@super.com', '{bcrypt}$2a$10$2Jk..KA5IiOebrrUgexVAOnY42jqryCRbMKT6FIWTXYJean7w2hUS', 'super', 'ADMIN', 'ACTIVE', 'NORMAL', true, NOW(), NOW(), '자기소개를 입력해주세요.', 'uploads/default.png', NOW());
 
-INSERT INTO users (email, password, nickname, role, user_status, account_status, is_email_verified, created_at, updated_at, introduce, profile_image_url)
-VALUES ('rhkdgh821@naver.com', '{bcrypt}$2a$10$fVn4lhyxvKSwaHQja3L4D.fTshkPTdWSEN8q9yEZ6OrmzwfoKwwB6', 'rhkdgh821', 'USER', 'ACTIVE', 'NORMAL', true, NOW(), NOW(), '자기소개를 입력해주세요.', 'uploads/default.png');
+INSERT INTO users (email, password, nickname, role, user_status, account_status, is_email_verified, created_at, updated_at, introduce, profile_image_url, last_login_at)
+VALUES ('rhkdgh821@naver.com', '{bcrypt}$2a$10$fVn4lhyxvKSwaHQja3L4D.fTshkPTdWSEN8q9yEZ6OrmzwfoKwwB6', 'rhkdgh821', 'USER', 'ACTIVE', 'NORMAL', true, NOW(), NOW(), '자기소개를 입력해주세요.', 'uploads/default.png', NOW());
 
 INSERT INTO board (name, description, created_at, updated_at)
 VALUES ('공지사항', '공지사항입니다.', NOW(), NOW());
