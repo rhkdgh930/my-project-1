a4c4015470285c7ec0e2c2032eda81b19886cddc
[PT1] 명광호 <rhkdgh99093015@gmail.com>
2026-04-28 20:44:56 +0900

    fix: Redis 삭제 실패를 성공으로 기록하지 않게 변경

--- src/main/java/com/example/my_project_1/outbox/handler/impl/UserAccountChangedHandler.java
+++ src/main/java/com/example/my_project_1/outbox/handler/impl/UserAccountChangedHandler.java
@@ -36,21 +36,12 @@ public class UserAccountChangedHandler implements OutboxHandler {
         Long userId = event.getUserId();
         UserAccountChangedType type = event.getType();
 
-        try {
-            if (type.shouldEvictCache()) {
-                redisUserContextService.evict(userId);
-            }
+        if (type.shouldEvictCache()) {
+            redisUserContextService.evict(userId);
+        }
 
-            if (type.shouldInvalidateToken()) {
-                redisTokenService.deleteRefreshTokenHash(userId);
-            }
-
-        } catch (Exception e) {
-            log.error(
-                    "[OUTBOX][USER_ACCOUNT_CHANGED][FAIL] userId={}",
-                    userId,
-                    e
-            );
+        if (type.shouldInvalidateToken()) {
+            redisTokenService.deleteRefreshTokenHash(userId);
         }
 
         log.debug("[OUTBOX][USER_ACCOUNT_CHANGED][SUCCESS] userId={} type={}", userId, type);
